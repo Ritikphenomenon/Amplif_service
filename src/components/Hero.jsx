7799aaaa7b7c51614f757dfd79de9e3cb295e625
elamandeep <deep.aman6174@gmail.com>
2024-03-05 18:06:12 +0530

    Created Footer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,22 @@
 export const Hero = () => {
     return (
-        <section className="bg-cover bg-center h-screen" style={{ backgroundImage: "url('./src/assets/Rectangle_full.png')" }}>
+        <section className="bg-cover bg-center h-screen flex items-center px-6 gap-3" style={{ backgroundImage: "url('./src/assets/Rectangle_full.png')" }}>
 
-            <div className="absolute left-10 top-20 flex items-center">
+         
 
-            <div className="bg-white/10 backdrop-blur-md p-2 text-white rounded-lg ">
-    <h1>Hello</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore deserunt, commodi sed molestias nostrum dolorum necessitatibus quam ducimus quae iure.</p>
-  </div>
-                
+                <div className="bg-white/10 backdrop-blur-md flex flex-col items-center justify-around p-6 text-white rounded-xl h-64 grow">
+                    <h1 className="text-yellow-600 text-3xl font-bold">Explore Our Expert Solution</h1>
+                    <div className="space-x-1 w-96 grid grid-cols-2 ">
+                        <button className="text-yellow-600 bg-black p-3  ">Paid Advertsing</button>
+                        <button className="text-yellow-600 bg-black p-3 ">Website Advertsing</button>
 
-            </div>
-            <div className="absolute bottom-20 right-6 flex items-center">
-                <img src='./src/assets/comp_animation.png' alt="Third Image" />
+                    </div>
+                </div>
+
+
+         
+            <div className="flex items-center mt-32">
+                <img src='./src/assets/comp_animation.png' className="" alt="Third Image" />
             </div>
         </section>
     );
